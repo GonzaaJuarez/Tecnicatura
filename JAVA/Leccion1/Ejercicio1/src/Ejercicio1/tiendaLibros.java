55fd7e578a2a18bee59cd859aac60c409b07882b
Gonzalo Juarez <gonzaloarieljuarez11@gmail.com>
2024-05-21 16:32:47 -0300

    ejercicioTiendaLibrosJava

---
+++ JAVA/Leccion1/Ejercicio1/src/Ejercicio1/tiendaLibros.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// Ejercicio Tienda de libros
+
+public class tiendaLibros {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proporciona el nombre del libro: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Proporciona el ID del libro: ");
+        int id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Proporciona el precio del libro: ");
+        double precio = Double.parseDouble(scanner.nextLine());
+        System.out.println("¿El envío es gratuito?(true/false): ");
+        boolean envio = Boolean.parseBoolean(scanner.nextLine());
+        System.out.println("Nombre del libro: " + nombre);
+        System.out.println("ID del libro: " + id);
+        System.out.println("Precio del libro: $" + precio);
+        System.out.println("¿Envío gratis? " + envio);
+    }
+}
