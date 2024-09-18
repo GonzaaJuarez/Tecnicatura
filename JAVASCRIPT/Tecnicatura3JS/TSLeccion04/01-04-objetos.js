4c072b694ead06b75e068c56ad802a07cf8b87a5
Gonzalo Juarez <gonzaloarieljuarez11@gmail.com>
2024-09-17 22:12:27 -0300

    Clase 5 JavaScript

--- JAVASCRIPT/Tecnicatura3JS/TSLeccion04/01-04-objetos.js
+++ JAVASCRIPT/Tecnicatura3JS/TSLeccion04/01-04-objetos.js
@@ -9,9 +9,21 @@ let persona = {
     apellido: "Juarez",
     email: "gonzaloarieljuarez11@gmail.com",
     edad: 24,
+    idioma: "es", // lo mejor es escribirlo en mayuscula
+    // por esto el toUpperCase para que de igual si esta en minúscula o en mayúscula
+    get lang(){
+        return this.idioma.toUpperCase(); // Convertir a mayúsculas
+    },
+    set lang(lang){
+        this.idioma = lang.toUpperCase(); // Convertir a mayúsculas
+    },
+    // Crear un método dentro de un objeto"
     //método o función en javascript
     nombreCompleto: function(){
         return this.nombre + " " + this.apellido;
+    },
+    get nombreEdad(){
+        return this.nombre + ", " + this.edad + " años";
     }
 }
 
@@ -29,7 +41,7 @@ console.log("Creando un nuevo objeto")
 let persona2 = new Object(); // crea un nuevo objeto en la memoria
 persona2.nombre = "Juan";
 persona2.direccion = "Roque Saenz Peña 254";
-persona2.telefono = "5492625786243"
+persona2.telefono = "5492686786243"
 console.log(persona2.telefono);
 
 
@@ -70,3 +82,101 @@ console.log(personaArray);
 console.log("Distintas formas de imprimir un objeto, FORMA 4:");
 let personaString = JSON.stringify(persona);
 console.log(personaString);
+
+
+console.log("METODO GET")
+console.log(persona.nombreEdad);
+
+console.log("METODO GET y SET EN IDIOMAS");
+persona.lang = "en"; // con el metodo set, directamente lo guarda en mayúsculas
+console.log(persona.lang);
+
+
+function Persona3(nombre, apellido, email){ // Constructor
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre + " " + this.apellido;
+    }
+}
+let padre = new Persona3("Sergio", "Perez", "sperez24@gmail.com");
+padre.telefono = "5492472859465"; // propiedad exlusiva del objeto "padre"
+console.log(padre.telefono);
+console.log(padre);
+console.log(padre.nombreCompleto());
+let madre = new Persona3("Marianela", "Tapia", "mtapia73@gmail.com");
+console.log(madre);
+console.log(madre.nombreCompleto());
+console.log(madre.telefono); // la propiedad no está definida
+
+
+// Diferentes formas de crear objetos
+// Todo despues del new es considerado objeto
+// CASO OBJETO 1
+let miObjeto1 = new Object(); // opción formal
+// CASO OBJETO 2
+let miObjeto2 = {}; // opción breve y recomendada
+
+// CASO STRING 1
+let miCadena = new String("Hola"); // opción formal
+// CASO STRING 2
+let miCadena2 = "Hola"; // opción breve y recomendada
+
+// CASO NÚMERO 1
+let miNumero = new Number(1); // opción formal
+// CASO NÚMERO 2
+let miNumero2 = 1; // opcion breve y recomendada
+
+// CASO BOOLEAN 1
+let miBoolean = new Boolean(false); // opción formal
+// CASO BOOLEAN 2
+let miBoolean2 = false; // opción breve y recomendada
+
+// CASO ARRAY 1
+let miArreglo = new Array(); // opción formal
+// CASO ARRAY 2
+let miArreglo2 = []; // opción breve y recomendada
+
+// CASO FUNCION 1
+let miFuncion = new Function(); // opción formal
+// CASO FUNCION 2
+let miFuncion2 = function(){}; // opción breve y recomendada
+
+// USO DE PROTOTYPE
+Persona3.prototype.telefono = "5492625786243";
+console.log(padre);
+console.log(madre);
+console.log(padre.telefono);
+console.log(madre.telefono);
+madre.telefono = "5492472217392";
+console.log(madre.telefono);
+
+
+// USO DE CALL
+let persona4 = {
+    nombre: 'Juan',
+    apellido: 'Lopez',
+    nombreCompleto2: function(titulo, telefono){
+        return titulo + ': ' + this.nombre + ' ' + this.apellido + ', ' + telefono;
+    },
+    nombreCompletoApply: function(){
+        return this.nombre + ' ' + this.apellido;
+    }
+}
+let persona5 = {
+    nombre: 'Maria',
+    apellido: 'Perez'
+}
+
+console.log(persona4.nombreCompleto2('Lic', '5492625686868'));
+console.log(persona4.nombreCompleto2.call(persona5, 'Ing.', '5492625357324'));
+
+// USO DE APPLY
+console.log(persona4.nombreCompletoApply());
+console.log(persona4.nombreCompletoApply.apply(persona5));
+
+console.log(persona4.nombreCompleto2.apply(persona5)) // no toma el título ni el teléfono
+// para ello hay que crear un arreglo con los valores de los parámetros
+let arreglo = ['Ing.', '5492625357324'];
+console.log(persona4.nombreCompleto2.apply(persona5, arreglo))
