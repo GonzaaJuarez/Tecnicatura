65485111a4d97338e7e5e23bea11f9411bb71952
Gonzalo Juarez <gonzaloarieljuarez11@gmail.com>
2024-09-19 16:41:16 -0300

    Clase 6 JAVA

--- JAVA/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ JAVA/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -3,8 +3,23 @@ package Operaciones;
 
 public class Aritmetica {
     // Atributos de la clase
+    // memoria heap, para atributos u objetos
     int a;
     int b;
+    
+    
+    // SOBRECARGA DE CONSTRUCTORES
+    // El constructor es un método especial
+    // CONSTRUCTOR 1
+    public Aritmetica(){ 
+        System.out.println("Se está ejecutando el constructor número 1");
+    }
+    // CONSTRUCTOR 2
+    public Aritmetica(int a, int b){ // no se usa var para atributos o parámetros
+        this.a = a;
+        this.b = b;
+        System.out.println("Se está ejecutando el constructor número 2");
+    }
     
     // METODO
     public void sumarNumeros(){
--- JAVA/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ JAVA/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -5,7 +5,14 @@ package Operaciones;
 
 public class PruebaAritmetica {
     public static void main(String[] args) {
-        Aritmetica aritmetica1 = new Aritmetica();
+        // memoria stack, almacena las variables
+        var a = 8; // variables locales
+        int b = 2; // varibales locales
+        // se puede usar var para variable slocales
+        
+        miMetodo(); // llamamos al método
+        
+        Aritmetica aritmetica1 = new Aritmetica(); // CONSTRUCTOR 1
         aritmetica1.a = 3;
         aritmetica1.b = 7;
         aritmetica1.sumarNumeros();
@@ -15,5 +22,20 @@ public class PruebaAritmetica {
         
         resultado = aritmetica1.sumarConArgumentos(4, 6);
         System.out.println("Resultado = " + resultado);
+        
+        System.out.println("Aritmetica1 a: " + aritmetica1.a);
+        System.out.println("Aritmetica1 b: " + aritmetica1.b);
+        
+        Aritmetica aritmetica2 = new Aritmetica(5, 8); // CONSTRUCTOR 2
+        System.out.println("Aritmetica2 a: " + aritmetica2.a);
+        System.out.println("Aritmetica2 b: " + aritmetica2.b);
+        
+        //aritmetica1 = null; // limpia o destruye (no se utiliza, ya que ocurre automaticamente)
+        //System.gc(); // limpia residuos del programa (no se utiliza, ya que ocurre automaticamente)
+    }
+    
+    public static void miMetodo(){
+        //a = 10; // la variable está limitada,no se puede trascender desde la clase main
+        System.out.println("Aquí hay otro método");
     }
 }
