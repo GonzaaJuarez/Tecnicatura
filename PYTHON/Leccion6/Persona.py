11809369b3ee21496b94f9c14149237134626a20
Gonzalo Juarez <gonzaloarieljuarez11@gmail.com>
2024-10-02 22:16:27 -0300

    Clase 6 Python

---
+++ PYTHON/Leccion6/Persona.py
@@ -0,0 +1,33 @@
+class Persona:  # Creamos una clase
+    def __init__(self, nombre, apellido, edad):  # Método Init Dunder
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        # no es común asignar valores por default
+
+    def mostrar_detalle(self):
+        print(f'Persona: {self.nombre} {self.apellido} de {self.edad} años')
+
+
+print(type(Persona))  # muestra que es una clase
+print(Persona)
+
+persona1 = Persona('Gonzalo', 'Juarez', 24)
+print(persona1.nombre)
+print(persona1.apellido)
+print(persona1.edad)
+
+persona2 = Persona('Leonel', 'Argente', 19)
+print(f'El objeto1 de la clase persona: {persona1.nombre} {persona1.apellido}, su edad es: {persona1.edad}')
+print(f'El objeto2 de la clase persona: {persona2.nombre} {persona2.apellido}, su edad es: {persona2.edad}')
+
+persona1.nombre = 'Franco'
+persona1.apellido = 'Rebeco'
+persona1.edad = 23
+
+print(f'El objeto1 modificado de la clase persona: {persona1.nombre} {persona1.apellido}, su edad es: {persona1.edad}')
+
+# Los atributos son: características.
+# Los métodos son: el comportamiento que van a tener los objetos (acciones).
+persona1.mostrar_detalle()
+persona2.mostrar_detalle()
