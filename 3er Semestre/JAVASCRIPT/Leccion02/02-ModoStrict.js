239d67d9fe2b46c69e3554864dc2058a7153eaea
GonzaJuarez <gonzaloarieljuarez11@gmail.com>
2025-04-14 22:37:58 -0300

    Clase 3t02 JavaScript - Modo strict y sobreescritura

---
+++ 3er Semestre/JAVASCRIPT/Leccion02/02-ModoStrict.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const x = 10;
+console.log(x);
+
+function miFunction(){
+    // "use strict" // tambien se puede generar dentro de una función
+    let y = 6;
+    console.log(y);
+}
+miFunction();
