44b99a32a85fcfedd2cbcaf00bb6d71f65e29554
GonzaJuarez <gonzaloarieljuarez11@gmail.com>
2025-08-14 16:46:29 -0300

    Clase 4s01 JAVASCRIPT - E-commerce Básico Parte 01

---
+++ 4to Semestre/E-COMMERCE/client/js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+  {
+    id: 1,
+    productName: "Banana",
+    price: 480,
+    quanty: 1,
+    img: "/4to Semestre/E-COMMERCE/client/media/banana.png",
+  },
+  {
+    id: 2,
+    productName: "Leche",
+    price: 950,
+    quanty: 1,
+    img: "/4to Semestre/E-COMMERCE/client/media/leche.png",
+  },
+  {
+    id: 3,
+    productName: "Mayonesa",
+    price: 950,
+    quanty: 1,
+    img: "/4to Semestre/E-COMMERCE/client/media/mayonesa.png",
+  },
+  {
+    id: 4,
+    productName: "Pollo",
+    price: 750,
+    quanty: 1,
+    img: "/4to Semestre/E-COMMERCE/client/media/pollo.png",
+  },
+];
